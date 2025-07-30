1657a6b8273217bd6193ddedc32149458a32e4d9
Sydoc-Code <benjamin.streich@sydoc.ch>
2025-07-30 11:37:18 +0200

    commit und so

--- app.py
+++ app.py
@@ -93,11 +93,6 @@ def post_login():
     #Check if user is logged in
     if 'username' not in session:
         return redirect(url_for("login"))
-<<<<<<< HEAD
- 
-=======
->>>>>>> 3d15ce06cf399b603672cf40df2a0d2be091dedf
-    
     logged_in_user = session.get('username', 'Unknown')
     scope = session.get('scope', 'Unknown')
 
