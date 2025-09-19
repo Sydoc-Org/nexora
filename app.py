4c81f840a63557b09975598848680412faa42370
Sydoc-Code <benjamin.streich@sydoc.ch>
2025-09-19 15:23:15 +0200

    password policy in reset password implemented aswell

--- app.py
+++ app.py
@@ -209,7 +209,9 @@ def set_new_password():
         return render_template("reset_password.html", error="Passwords do not match")
     if not new_password or not confirm_password:
         return render_template("reset_password.html", error="All Fields must be filled")
-    
+    if not re.search('^\S{8,200}$', new_password):
+        return render_template("reset_password.html", error="New password has to be atleast 8 characters long, with no whitespaces")
+
     conn_str = (
         f'DRIVER={{SQL Server}};'
         f'SERVER={DB_SERVER},1433;'
