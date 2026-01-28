f3cdf8e486825fcc72df41d47fdf8abdaac3416a
benstreich <ben.streich@icloud.com>
2026-01-28 13:54:31 +0100

    powershell script update

--- logs/csvLogs_toDB.ps1
+++ logs/csvLogs_toDB.ps1
@@ -2,31 +2,27 @@ $nexoraLogFolder = 'D:\sydoc\nexora\logs'
 $dbServer = 'PRDSQL01'
 $dbName = 'nexora'
 $dbTable = 'Logs'
-Get-ChildItem -Directory $nexoraLogFolder | ForEach-Object {
+
+Get-ChildItem -Directory $nexoraLogFolder | ForEach-Object  {
     Import-Csv ($_.FullName + '\nexora_logs.csv') | ForEach-Object {
-        $query = @"
-        INSERT INTO $($dbTable) (SessionID,
-        RequestIpAddress,
-        UserID,
-        Username,
-        HttpRequestMethod,
-        Path,
-        HttpResponseCode,
-        Args,
-        durationSeconds
-        )
-        VALUES (
-            '$($_.SessionID)',
-            '$($_.RequestIpAddress)',
-            '$($_.UserID)',
-            '$($_.Username)',
-            '$($_.HttpRequestMethod)',
-            '$($_.Path)',
-            '$($_.HttpResponseCode)',
-            '$($_.Args)',
-            '$($_.durationSeconds)'
-        )
-"@
-        Invoke-Sqlcmd -ServerInstance $dbServer -Database $dbName -Query $query -TrustServerCertificate
-    }
+            $query = "INSERT INTO $dbTable (SessionID, RequestIpAddress, UserID, Username, HttpRequestMethod, Path, HttpResponseCode, Args, durationSeconds) VALUES (@SessionID, @RequestIpAddress, @UserID, @Username, @HttpRequestMethod, @Path, @HttpResponseCode, @Args, @durationSeconds)"
+            $connection = New-Object System.Data.SqlClient.SqlConnection("Server=$dbServer;Database=$dbName;Integrated Security=True;TrustServerCertificate=True")
+            $cmd = $connection.CreateCommand()
+            $cmd.CommandText = $query
+            $cmd.Parameters.AddWithValue("@SessionID", $_.SessionID) | Out-Null
+            $cmd.Parameters.AddWithValue("@RequestIpAddress", $_.RequestIpAddress) | Out-Null
+            $cmd.Parameters.AddWithValue("@UserID", $_.UserID) | Out-Null
+            $cmd.Parameters.AddWithValue("@Username", $_.Username) | Out-Null
+            $cmd.Parameters.AddWithValue("@HttpRequestMethod", $_.HttpRequestMethod) | Out-Null
+            $cmd.Parameters.AddWithValue("@Path", $_.Path) | Out-Null
+            $cmd.Parameters.AddWithValue("@HttpResponseCode", $_.HttpResponseCode) | Out-Null
+            $cmd.Parameters.AddWithValue("@Args", $_.Args) | Out-Null
+            $cmd.Parameters.AddWithValue("@durationSeconds", $_.durationSeconds) | Out-Null
+            $connection.Open()
+            $cmd.ExecuteNonQuery()
+            $connection.Close()
+
+        }
+    Remove-Item $_.FullName -Recurse -Force
 }
+
