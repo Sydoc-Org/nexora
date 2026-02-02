9980b60a3d50213ca4f080789c64a5c4b6148f6d
benstreich <ben.streich@icloud.com>
2026-02-02 08:55:01 +0100

    computed columns on some horrible nvarchar date columns, so every query doesnt have to do a conversion. instead sql server handles that on update or insert

--- sql/processConfig/DocFieldSearch/IX_PriveraInvoice.sql
+++ sql/processConfig/DocFieldSearch/IX_PriveraInvoice.sql
@@ -1,7 +1,8 @@
 CREATE NONCLUSTERED INDEX [IX_PriveraInvoice_NexoraDocFieldSearch] ON [dbo].[PriveraInvoice]
 (
 	[WID] ASC,
-	[ImportTime] ASC
+	[ImportTime] ASC,
+	[ExportDate] ASC
 )
 INCLUDE([DocType],[Barcode],[CRD_NR],[CRD_NAME_1],[BankPK],[GrossAmount],[NetAmount],[VatAmount],[DocCurrency],[InvoiceNR],[ISTEC],[ESR],[BestellNummer],[Mandant],[DocSource],[EigentuemerNr],[LiegenschaftsNr]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
