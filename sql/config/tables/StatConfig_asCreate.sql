e2d88eb3388c54aeedd3ca911bd5c8be1c90401a
benstreich <ben.streich@icloud.com>
2026-02-03 11:44:32 +0100

    chat function, sql scripts, batchimport

---
+++ sql/config/tables/StatConfig_asCreate.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[StatConfig](
+	[ProcessName] [nvarchar](100) NULL,
+	[TableName] [nvarchar](100) NULL,
+	[ExportColumn] [nvarchar](100) NULL,
+	[additionalCondition] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
