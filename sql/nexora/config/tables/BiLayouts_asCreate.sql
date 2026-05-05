f7809dc8ad00b8f52189812ff1ab7c6c3a511b04
benstreich <ben.streich@icloud.com>
2026-05-05 18:07:40 +0200

    removed non active table sql scripts

--- sql/nexora/config/tables/BiLayouts_asCreate.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[BiLayouts] (
-    [UserID]    INT            NOT NULL,
-    [LayoutJSON] NVARCHAR(MAX)  NOT NULL,
-    [UpdatedAt]  DATETIME2      NOT NULL DEFAULT SYSUTCDATETIME(),
-    CONSTRAINT [PK_BiLayouts] PRIMARY KEY CLUSTERED ([UserID] ASC)
-);
